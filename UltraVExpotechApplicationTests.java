566dfb8b92bfd98e9f0a98da101af59a88c89c84
ana <anamachado290506@gmail.com>
2026-05-29 03:55:52 -0300

    Add files via upload

---
+++ UltraVExpotechApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ultravexpotech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltraVExpotechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
